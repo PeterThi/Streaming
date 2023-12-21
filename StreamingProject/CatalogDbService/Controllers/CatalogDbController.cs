efcd31ec33c258f8080de3621134de62c5113bec
PeterThi <pete9817@gmail.com>
2023-12-21 14:05:27 +0100

    init

---
+++ StreamingProject/CatalogDbService/Controllers/CatalogDbController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using MySqlConnector;
+using Dapper;
+namespace CatalogDbService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogDbController : ControllerBase
+    {
+
+        private IDbConnection CatalogDatabaseConnection = new MySqlConnection("Server=catalogDatabase;Database=catalog-database;Uid=add-bruger;Pwd=C@ch3d1v2;");
+
+
+        public CatalogDbController()
+        {
+
+        }
+
+        [HttpPost]
+        public void Post(string titleFromParam, int lengthFromParam, string reviewFromParam)
+        {
+            CatalogDatabaseConnection.Open();
+            var tables = CatalogDatabaseConnection.Query<string>("SHOW TABLES LIKE Movies");
+            if (!tables.Any())
+            {
+                CatalogDatabaseConnection.Execute("CREATE TABLE Movies(Title string, length int, review string)");
+            }
+            CatalogDatabaseConnection.Execute("INSERT INTO Movies(Title,length,review) VALUES (@title, @length, @review", new { title = titleFromParam, length = lengthFromParam, review = reviewFromParam });
+
+
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            IEnumerable<string> resultList = CatalogDatabaseConnection.Query<string>("SELECT title from Movies");
+            resultList.Append(CatalogDatabaseConnection.Query<string>("SELECT length from Movies").ToString());
+            foreach (var item in resultList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            return resultList;
+
+        }
+    }
+}
